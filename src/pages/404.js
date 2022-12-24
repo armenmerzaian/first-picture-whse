823b80ba5eeac6a4055350c8c9ccd63b3c6d87c5
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2022-12-24 09:46:34 -0500

    dericks a bitch

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,12 +7,19 @@ const NotFoundPage = () => {
     <>
       <Navigation />
       <div className="pt-[188px] bg-secondary-dark h-full flex flex-col gap-4 justify-center items-center py-28">
-        <span className="font-bold text-7xl text-primary-55">404</span>
-        <span className="font-regular text-4xl text-tertiary-light/38">Page Not Found</span>
+        <span className="font-light text-[176px] text-primary-55">404</span>
+        <span className="font-regular text-[23px] text-tertiary-light/38">
+          Dericks a Bitch
+        </span>
+        <span className="font-regular text-[16px] text-tertiary-light/38">
+          Love, 
+          <br/>
+          Araz
+        </span>
       </div>
-      <Footer /> 
+      <Footer />
     </>
-  )
+  );
 }
 
 export default NotFoundPage
